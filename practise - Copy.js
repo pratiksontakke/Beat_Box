461ab0918c6b9fe876937cec473384f0af7a82b2
Pratik Sontakke <62612283+pratiksontakke@users.noreply.github.com>
2022-04-14 15:10:18 +0530

    new file
     practise-copy in project

---
+++ practise - Copy.js
@@ -0,0 +1,31 @@
+/*
+// OBJECT LOGIC
+
+beatsObj = {
+	65: "Hello 65",
+	83: "Hello 83",
+	68: "Hello 68",
+	70: "Hello 70",
+	71: "Hello 71",
+	72: "Hello 72",
+	74: "Hello 74",
+	75: "Hello 75",
+	76: "Hello 76",
+};
+// console.log(Object.keys(beatsObj));
+// console.log((beatsObj[65]));
+// ARRAY LOGIC
+beatsArr = [
+	{ 65: "Hello 65" },
+	{ 83: "Hello 83" },
+	{ 68: "Hello 68" },
+	{ 70: "Hello 70" },
+	{ 71: "Hello 71" },
+	{ 72: "Hello 72" },
+	{ 74: "Hello 74" },
+	{ 75: "Hello 75" },
+	{ 76: "Hello 76" },
+];
+
+// console.log(beatsArr[0]);
+*/
